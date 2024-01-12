107bc290541165af95436a6dcfddfb5d11d91af4
Muffin <muffin@mailbox.org>
2024-01-11 20:25:35 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# @turbowarp/minierlog
+
+Replacement for `minilog` for TurboWarp.
+
+~530 bytes minified, ~372 bytes gzipped
---
+++ index.js
@@ -0,0 +1,52 @@
+// only use colors in non-browser environments
+const addColors = typeof document === 'undefined';
+
+const RESET = addColors ? '\u001b[0m' : '';
+const GRAY = addColors ? '\u001b[90m' : '';
+const BLUE = addColors ? '\u001b[34m' : '';
+const CYAN = addColors ? '\u001b[36m' : '';
+const YELLOW = addColors ? '\u001b[33m' : '';
+const RED = addColors ? '\u001b[31m' : '';
+
+const DEBUG = `${BLUE}debug${RESET}`;
+const INFO = `${CYAN}info${RESET}`;
+const WARN = `${YELLOW}warn${RESET}`;
+const ERROR = `${RED}error${RESET}`;
+
+let enabled = false;
+
+const createLog = (namespace = '') => {
+    const log = (childNamespace) => createLog(`${namespace} ${childNamespace}`);
+
+    const formattedNamespace = namespace ? [`${GRAY}${namespace}${RESET}`] : [];
+
+    log.debug = log.log = (...args) => {
+        if (enabled) console.debug(...formattedNamespace, DEBUG, ...args);
+        return log;
+    };
+    log.info = (...args) => {
+        if (enabled) console.info(...formattedNamespace, INFO, ...args);
+        return log;
+    };
+    log.warn = (...args) => {
+        if (enabled) console.warn(...formattedNamespace, WARN, ...args);
+        return log;
+    };
+    log.error = (...args) => {
+        if (enabled) console.error(...formattedNamespace, ERROR, ...args);
+        return log;
+    };
+
+    return log;
+};
+
+createLog.enable = () => {
+    enabled = true;
+    return createLog;
+};
+createLog.disable = () => {
+    enabled = false;
+    return createLog;
+};
+
+module.exports = createLog;
